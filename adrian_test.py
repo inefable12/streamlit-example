f223020360cedabeee6db7b1d5bf85d9a5247b37
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2024-02-26 21:49:02 -0300

    adrian_test.py

--- adrian_test.py
+++ adrian_test.py
@@ -39,17 +39,11 @@ st.altair_chart(alt.Chart(df, height=700, width=700)
         size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
     ))
 
-"""
-import streamlit as st
-import pandas as pd
-#import matplotlib.pyplot as plt
 
 st.write("Estadística de goles de Messi")
 
 # Cargar los datos desde la página web
 pagina = 'https://players.fcbarcelona.com/es/jugador/548-messi-lionel-andres-messi-cuccitini'
 tablas = pd.read_html(pagina)
-
-# Crear la aplicación web con Streamlit
 st.write(tablas[0].head(5))
-"""
+
