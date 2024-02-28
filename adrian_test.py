84cd4772ed5525ce9602963e3c2ab78461bc5487
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2024-02-28 09:41:21 -0500

    Update adrian_test.py

--- adrian_test.py
+++ adrian_test.py
@@ -65,10 +65,17 @@ def matplotlib():
 
     """)
     st.write("Esto da como resultado:")  
-    ds = pd.DataFrame(x,y) ({'x': [1, 2, 3, 4, 5], 'y': [2, 4, 6, 8, 10]})
-    st.write(ds)
+    #ds = pd.DataFrame(x,y) ({'x': [1, 2, 3, 4, 5], 'y': [2, 4, 6, 8, 10]})
+    #st.write(ds)
+    x = [1, 2, 3, 4, 5]
+    y = [2, 4, 6, 8, 10]
+    df = pd.DataFrame({
+    "x": x,
+    "y": y,
+    "idx": indices,
+    "rand": np.random.randn(num_points),})
     
-    st.altair_chart(alt.Chart(ds, height=700, width=700)
+    st.altair_chart(alt.Chart(df, height=700, width=700)
         .mark_point(filled=True)
         .encode(
             x=alt.X("x", axis=None),
