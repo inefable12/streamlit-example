5b33de245d5caac953e77db358e072425ec146ae
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2024-02-28 08:54:40 -0500

    Update adrian_test.py

--- adrian_test.py
+++ adrian_test.py
@@ -57,6 +57,16 @@ def matplotlib():
 
     """)
     st.write("Esto da como resultado:")
+    import matplotlib.pyplot as plt
+
+    x = [1, 2, 3, 4, 5]
+    y = [2, 4, 6, 8, 10]
+    
+    plt.plot(x, y)
+    plt.title('Gráfico de ejemplo')
+    plt.xlabel('Eje X')
+    plt.ylabel('Eje Y')
+    plt.show()
 
 def evaluacion():
     st.title('Evaluación')
