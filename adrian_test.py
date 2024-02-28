614110446149e0763dac780ff85aa58584baa148
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2024-02-28 08:01:08 -0500

    Update adrian_test.py

--- adrian_test.py
+++ adrian_test.py
@@ -10,28 +10,44 @@ def contenido():
     st.write("Aquí encontrarás contenido relacionado con la tecnología.")
     # Puedes agregar contenido adicional aquí
 
-def redes_sociales():
+def pandas():
     st.title("Pandas")
     st.write("Síguenos en nuestras redes sociales para mantenerte actualizado.")
     # Puedes agregar contenido adicional aquí
 
-def redes_sociales():
+def matplotlib():
     st.title("Matplotlib")
     st.write("Síguenos en nuestras redes sociales para mantenerte actualizado.")
     # Puedes agregar contenido adicional aquí
 
+def evaluacion():
+    st.title('Formulario de Google')
+
+    # Inserta el código HTML del formulario de Google
+    st.components.v1.html(
+        """
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc3N9Lanr3N0tkoHmHM8ClTgbM3B-Tpwk7m7XwveeuylYskkA/viewform?usp=sf_link" 
+                width="640" height="480" frameborder="0" marginheight="0" marginwidth="0">Cargando...</iframe>
+        """,
+        height=600,
+    )
+
+#https://docs.google.com/forms/d/e/1FAIpQLSc3N9Lanr3N0tkoHmHM8ClTgbM3B-Tpwk7m7XwveeuylYskkA/viewform?usp=sf_link
+
 def main():
-    st.sidebar.title("Navegación")
-    selection = st.sidebar.radio("Ir a:", ["Inicio", "Numpy", "Pandas", "Matplotlib"])
+    st.sidebar.title("Verano 2024")
+    selection = st.sidebar.radio("Ir a:", ["Inicio", "Numpy", "Pandas", "Matplotlib", "Evaluación])
     st.sidebar.image("https://upload.wikimedia.org/wikipedia/commons/9/9c/Alan_Turing_in_watercolour.png",
                  caption="Acuarela de Alan Turing, generada mediante inteligencia artificial, considerado el padre de la misma")
 
     if selection == "Inicio":
         inicio()
-    elif selection == "Contenido":
-        contenido()
-    elif selection == "Redes Sociales":
-        redes_sociales()
+    elif selection == "Numpy":
+        numpy()
+    elif selection == "Matplotlib":
+        matplotlib()
+    elif selection == "Evaluación":
+    evaluacion()
 
 if __name__ == "__main__":
     main()
