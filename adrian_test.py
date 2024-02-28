6265ff9d287510be4594ce9dfb28f3b330c26f06
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2024-02-28 07:48:03 -0500

    Update adrian_test.py

--- adrian_test.py
+++ adrian_test.py
@@ -6,18 +6,23 @@ def inicio():
     # Puedes agregar contenido adicional aquí
 
 def contenido():
-    st.title("Contenido")
+    st.title("Numpy")
     st.write("Aquí encontrarás contenido relacionado con la tecnología.")
     # Puedes agregar contenido adicional aquí
 
 def redes_sociales():
-    st.title("Redes Sociales")
+    st.title("Pandas")
+    st.write("Síguenos en nuestras redes sociales para mantenerte actualizado.")
+    # Puedes agregar contenido adicional aquí
+
+def redes_sociales():
+    st.title("Matplotlib")
     st.write("Síguenos en nuestras redes sociales para mantenerte actualizado.")
     # Puedes agregar contenido adicional aquí
 
 def main():
     st.sidebar.title("Navegación")
-    selection = st.sidebar.radio("Ir a:", ["Inicio", "Contenido", "Redes Sociales"])
+    selection = st.sidebar.radio("Ir a:", ["Inicio", "Numpy", "Pandas", "Matplotlib])
     st.sidebar.image("https://upload.wikimedia.org/wikipedia/commons/9/9c/Alan_Turing_in_watercolour.png",
                  caption="Acuarela de Alan Turing, generada mediante inteligencia artificial, considerado el padre de la misma")
 
