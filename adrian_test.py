2c4b9fa8df6d26cab18ee0375fafe38c2588fecc
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2024-02-28 09:41:54 -0500

    Update adrian_test.py

--- adrian_test.py
+++ adrian_test.py
@@ -69,11 +69,7 @@ def matplotlib():
     #st.write(ds)
     x = [1, 2, 3, 4, 5]
     y = [2, 4, 6, 8, 10]
-    df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),})
+    df = pd.DataFrame({"x": x,"y": y,"idx": indices,"rand": np.random.randn(num_points),})
     
     st.altair_chart(alt.Chart(df, height=700, width=700)
         .mark_point(filled=True)
